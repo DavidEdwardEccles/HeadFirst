852f60677fe0adf672962d7720c3205d9c459e47
David Eccles <davideccles@hotmail.com>
2020-07-30 14:42:04 +0100

    Animal Test

---
+++ Chapter 8/myAnimalList.java
@@ -0,0 +1,13 @@
+public class myAnimalList {
+
+    private Animal[] animals = new Animal[5];
+    private int nextIndex = 0;
+
+    public void add (Animal a) {
+        if (nextIndex < animals.length) {
+            animals[nextIndex] = a;
+            System.out.println("Animal added at " + nextIndex);
+            nextIndex++;
+        }
+    }
+}
