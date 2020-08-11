5b06876d30fa3d1bb165fcae9362105d12a71c3d
DavidEdwardEccles <69000721+DavidEdwardEccles@users.noreply.github.com>
2020-08-11 16:17:43 +0100

    added 11082020

---
+++ Ex 4/ElectricGuitar.java
@@ -0,0 +1,34 @@
+public class ElectricGuitar {
+    public static void main(String[] args) {
+    
+    class Guitar {
+        String brand;
+        int numOfPickups;
+        boolean rockStarUsesIt;
+
+    String getBrand () {
+        return brand;
+    }
+
+    void setBrand(String aBrand) {
+        brand = aBrand;
+    }
+
+    int getNumOfPickups () {
+        return numOfPickups;
+    }
+
+void setNumOfPickups(int num) {
+    numOfPickups = num;
+}
+
+    boolean getRockStarUsesIt() {
+        return rockStarUsesIt;
+    }
+
+    void setRockStarUsesIt(boolean yes) {
+        rockStarUsesIt = yes;
+    }
+}
+}
+}
